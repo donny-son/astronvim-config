87ec0a87c64b04a80f595721e53efe7446991102
Dongook Son <60206749+donny-son@users.noreply.github.com>
2023-03-18 23:18:09 +0900

    Init astronvim config

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,20 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { -- further customize the options set by the community
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,53 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    'tzachar/cmp-tabnine',
+    build = './install.sh',
+    dependencies = 'hrsh7th/nvim-cmp',
+    -- config = "plugins.configs.tabnine"
+  },
+  {
+    "kylechui/nvim-surround",
+    event = "User AstroFile",
+    config = function()
+      require('nvim-surround').setup({})
+    end
+  },
+  {
+    "declancm/cinnamon.nvim",
+    event = "User AstroFile",
+    config = function()
+      require('cinnamon').setup({})
+    end
+  },
+  {
+    "mechatroner/rainbow_csv",
+    event = "User AstroFile",
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && yarn install",
+    event = "User AstroFile",
+    config = function()
+      vim.g.mkdp_filetypes = { "markdown" };
+      vim.g.mkdp_auto_start = 0;
+      vim.g.mkdp_auto_close = 1;
+      vim.g.mkdp_refresh_slow = 0;
+      vim.g.mkdp_command_for_global = 0;
+      vim.g.mkdp_auto_start = 0;
+      vim.g.mkdp_auto_close = 1;
+      vim.g.mkdp_refresh_slow = 0;
+      vim.g.mkdp_command_for_global = 0;
+      vim.g.mkdp_open_to_the_world = 0;
+      vim.g.mkdp_open_ip = '';
+      vim.g.mkdp_browser = '';
+      vim.g.mkdp_echo_preview_url = 0;
+      vim.g.mkdp_browserfunc = '';
+      vim.g.mkdp_markdown_css = '';
+      vim.g.mkdp_highlight_css = '';
+      vim.g.mkdp_port = '';
+      vim.g.mkdp_page_title = '${name}';
+      vim.g.mkdp_theme = 'dark';
+      vim.keymap.set("n", "<leader>md", "<cmd>MarkdownPreviewToggle<cr>");
+    end
+  },
 }
