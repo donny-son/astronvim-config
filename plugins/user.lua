54569e9f751e8d72bd4e0dbc6992eed6f074cf2c
Dongook Son <60206749+donny-son@users.noreply.github.com>
2023-06-01 11:20:32 +0900

    Add laravel.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -56,5 +56,31 @@ return {
     "michaelb/sniprun",
     event = "User AstroFile",
     build = "bash install.sh",
+  },
+  {
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "tpope/vim-dotenv",
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    keys = {
+      { "<leader>la", ":Laravel artisan<cr>" },
+      { "<leader>lr", ":Laravel routes<cr>" },
+      {
+        "<leader>lt",
+        function()
+          require("laravel.tinker").send_to_tinker()
+        end,
+        mode = "v",
+        desc = "Laravel Application Routes",
+      },
+    },
+    event = { "VeryLazy" },
+    config = function()
+      require("laravel").setup()
+      require("telescope").load_extension "laravel"
+    end,
   }
 }
